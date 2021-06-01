177a09e6647df35b9e791f15e9e5721b356e5179
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-01 21:12:19 +0530

    [Rikesh] added message whether employee is absent or present

--- Employee.java
+++ Employee.java
@@ -1,6 +1,12 @@
 public class Employee{
 	public static void main(String [] args){
 	System.out.println("Welcome to Employee Wage Computation Program");
-
 	}
+	int attendanceCheck = (int)(Math.random()*2);
+	if (attendanceCheck == 0){
+		System.out.println("Employee is Absent");
+		else{
+		System.out.println("Employee is Present");
+	}
+}
 }
