2b9877dfc49f7462442396bf565463f599d79c8d
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-01 22:03:06 +0530

    [Rikesh] added for loop to calculate employee monthly wage

--- Employee.java
+++ Employee.java
@@ -2,16 +2,21 @@ public class Employee{
 	static final int isFullTime = 1;
 	static final int isPartTime = 2;
 	static final int wageRatePerHour = 20;
+	static int workingDayPerMonth = 20;
+	static int employeeMonthlyWage = 0;
 	int employeeDailyWage = 0;
 
 	void employeeWageCalculation( int workRatePerHour ) {
 		 employeeDailyWage = wageRatePerHour * workRatePerHour;
+		 employeeMonthlyWage += employeeDailyWage;
 		 System.out.println("Employee Daily wage is : " +employeeDailyWage +"$");
 	}
 
 	public static void main(String [] args){
 	Employee employee = new Employee();
 	System.out.println("Welcome to Employee Wage Computation Program");
+
+	for (int day = 0; day<workingDayPerMonth; day++ ){
 	int attendanceCheck = (int)(Math.random()*3);
 	switch (attendanceCheck){
 			case isFullTime:
@@ -25,5 +30,7 @@ public class Employee{
 			default:
 			employee.employeeWageCalculation(0);
 		     }
+		}
+	  System.out.println("Employee Monthly wage is :" +employeeMonthlyWage  +"$");
 }
 }
